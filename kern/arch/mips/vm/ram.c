4a21cc2f866b970997292e0c8df73bd41e9c93cd
Srikanth Reddy Malipatel <srikk595@gmail.com>
2016-04-01 05:54:47 -0400

    removed log

--- kern/arch/mips/vm/ram.c
+++ kern/arch/mips/vm/ram.c
@@ -97,9 +97,6 @@ ram_stealmem(unsigned long npages)
 	size_t size;
 	paddr_t paddr;
 
-#ifndef OPT_DUMBVM
-	kprintf("**** calling ram stealmem **** \n");
-#endif
 	size = npages * PAGE_SIZE;
 
 	if (firstpaddr + size > lastpaddr) {
